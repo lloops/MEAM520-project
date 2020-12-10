619b8db32c9cca9b6ff157fae9b73cbf2ba6a387
QZJGeorge <74877573+QZJGeorge@users.noreply.github.com>
2020-12-09 21:17:24 -0500

    Update calculateFK.py

--- calculateFK.py
+++ calculateFK.py
@@ -39,28 +39,21 @@ class calculateFK():
         jointPositions = np.zeros((6,3))
         T0e = np.identity(4)
 
-        ###---------------------###
         ## establish DH parameters
         a_lst = [0, self.L2, self.L3, 0, 0]
         alp_lst = [-math.pi/2, 0, 0, -math.pi/2, 0]
-
         d_lst = [self.L1, 0, 0, 0, self.L4 + self.L5]
-
         q = np.array(q).reshape(6)
 
         theta_lst = [q[0], q[1] - math.pi/2, q[2] + math.pi/2, q[3] - math.pi/2, q[4]]
 
-        ###---------------------###
         #position of center of joint 5 wrt frame 4
-        p_4 = np.array(
-                [0, 0, 34, 1]
-                )
+        p_4 = np.array([0, 0, 34, 1])
 
-
+        # compute the transformation matrix of the gripper
         for i in range(5):
 
             T0e = T0e.dot( self.getA( a_lst[i], alp_lst[i], d_lst[i], theta_lst[i] ) )
-
             pos = T0e[:-1, -1]
 
             #to calculate position of (the center of) joint 5,
@@ -76,20 +69,17 @@ class calculateFK():
         return jointPositions, T0e
 
 
-
+    # input: DH parameters
+    # output: DH matrix
     def getA(self, a, alp, d, theta):
 
         sin_theta, cos_theta = math.sin(theta), math.cos(theta)
         sin_alp, cos_alp = math.sin(alp), math.cos(alp)
 
         A = np.array([
-                         [cos_theta, -sin_theta * cos_alp, sin_theta * sin_alp, a*cos_theta], #row1
-                         [sin_theta, cos_theta * cos_alp, -cos_theta * sin_alp, a*sin_theta],  #row2
-                         [0, sin_alp, cos_alp, d],
-                         [0, 0, 0, 1]
-
-                         ])
+                        [cos_theta, -sin_theta * cos_alp, sin_theta * sin_alp, a*cos_theta],
+                        [sin_theta, cos_theta * cos_alp, -cos_theta * sin_alp, a*sin_theta],
+                        [0, sin_alp, cos_alp, d],
+                        [0, 0, 0, 1]])
 
         return A
-
-        
