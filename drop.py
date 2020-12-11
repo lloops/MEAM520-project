01f4ccea06187c58f5c24d214758e0945cbf9192
zxy9815 <s1998zxy@hotmail.com>
2020-12-11 14:24:40 -0500

    final drop

--- drop.py
+++ drop.py
@@ -26,31 +26,34 @@ def drop(qstart, color, pose, list_len, object_pose):
     # Compute transformation matrix of drop pose
     #############################################
 
-    # Initialize FK and offsets btw Gripper center and object center
+    # Initialize FK and offsets between Gripper center and object center (currently grabbed by the gripper)
     FK = calculateFK()
     X_offset = 0
     Y_offset = 0
 
-    # Height and dropping location
-    # Case no object is present at dropping area, drop to assigned location
+    # 3 drop points p1, p2, p3; (or drop "areas" since the object will not land at exactly the same x,y)
+    # stack a max of 3 objects at p1, stack a max of 2 objects at p2, keep stacking onto p3 for the subsequent objects grabbed
+    
+    # Height and drop location
+    # Case no object is present at the first drop point p1, drop to p1 area
     if(len(pose) == 0 or list_len == 0):
         height = 50.0
         d_drop0_blue = np.array([120., 520., height, 1]).reshape((4,1))
         d_drop0_red = np.array([-120., -520., height, 1]).reshape((4,1))
 
-    # Case already 3 objects at dropping area, drop to another location
+    # Case already 3 objects at p1, switch to the next drop point p2
     elif(list_len == 3):
         height = 50.0
         d_drop0_blue = np.array([70., 520., height, 1]).reshape((4,1))
         d_drop0_red = np.array([-70., -520., height, 1]).reshape((4,1))
 
-    # Case already 5 objects at dropping area, drop to the 3rd location
+    # Case already 2 objects at p2 (a total of 5 objects on the platform), drop to the 3rd location p3
     elif(list_len == 5):
         height = 50.0
         d_drop0_blue = np.array([120., 470., height, 1]).reshape((4,1))
         d_drop0_red = np.array([-120., -470., height, 1]).reshape((4,1))
 
-    # else drop on top of the previously dropped object
+    # else drop on top of the previously dropped object (the top object on the stack at drop point p_i)
     else:
         height = pose[-1] + 28.0
         d_drop0_blue = np.array([pose[0], pose[1], height, 1]).reshape((4,1))
@@ -69,12 +72,12 @@ def drop(qstart, color, pose, list_len, object_pose):
 
         d_drop1_blue = d_drop1_blue[0:3,:]
 
-        # Angle btw X_base axis and drop location
+        # Angle btw X_base axis and drop location (vector)
         d_drop1_blue_xy = np.array([d_drop1_blue[0], d_drop1_blue[1], 0])
         cos_theta = ((np.array([1,0,0]).dot(d_drop1_blue_xy)) / np.linalg.norm(d_drop1_blue_xy))[0]
         theta = np.arccos(cos_theta)
 
-        # Update Height
+        # Update Height and calculate x,y offset based on relative position of the grabbed object wrt the gripper
         if (len(object_pose)):
             obj_0 = np.matmul(T01, np.array(object_pose))
             Z_obj = obj_0[2,-1]
@@ -87,12 +90,13 @@ def drop(qstart, color, pose, list_len, object_pose):
             X_offset = positions[-1,0] - X_obj
             Y_offset = positions[-1,1] - Y_obj
 
-
+        # Te1: drop off pose ; apply the offset to the desired gripper position for dropping the object at drop point p_i
         Te1 = np.array([[np.sin(theta), 0, cos_theta, d_drop1_blue[0,0] + X_offset],
                         [cos_theta, 0, -np.sin(theta), d_drop1_blue[1,0] - 3],
                         [0, 1, 0, height],
                         [0, 0, 0, 1]])
 
+    # same implemenation as the 'blue'
     elif(str(color).lower() == 'red'):
 
         T01 = np.array([[1, 0, 0, 200],
@@ -160,9 +164,10 @@ def drop(qstart, color, pose, list_len, object_pose):
     Tinter1 = deepcopy(Te1)
 
     Tinter1[0:3,-1] = np.array([Te1[0,-1] - 30, Te1[1,-1] + 70, height + 70])
-
+    
     q_inter, isPos = IK.inverse(Tinter1)
 
+    # Add intermediate pose
     q_inter = np.append(np.ravel(q_inter)[range(5)],0)
     q_inter[-2] = -np.pi/2
     path.append(q_inter)
@@ -191,7 +196,7 @@ def drop(qstart, color, pose, list_len, object_pose):
         path.append(q_release)
 
     return path
-
+    
 
 
 ##########################################################################
@@ -213,7 +218,7 @@ def drop_object(lynx, color, object_index, value):
     qStart, vel = lynx.get_state()
     [name, pose, twist] = lynx.get_object_state()
 
-    # get pose of object currently dropping
+    # get pose of the grabbed object to be dropped
     if(object_index == -1):
         object_pose = []
     else:
@@ -222,10 +227,13 @@ def drop_object(lynx, color, object_index, value):
     # get pose of all cubes on green plate
     new_list = []
     if(color == "blue"):
-
+        #list: information of all objects on the green platform 
         list = [[pose[i], pose[i][2,3]] for i in range(len(name)) if (pose[i][1,3]>0 and pose[i][1,3] > 440 and pose[i][2,3]>0) ]
+        
+        # new_list: pose and height of the object(s) stacked at p2 
         if(len(list) >= 3 and len(list) < 5):
             new_list = [[pose[i], pose[i][2,3]] for i in range(len(name)) if (pose[i][1,3]>0 and pose[i][1,3] > 440 and pose[i][2,3]>0 and pose[i][0,3] < 95) ]
+        # new_list: pose and height of the object(s) stacked at p3
         elif(len(list) >= 5):
             new_list = [[pose[i], pose[i][2,3]] for i in range(len(name)) if (pose[i][1,3]>0 and pose[i][1,3] > 440 and pose[i][2,3]>0 and pose[i][1,3] < 490) ]
 
@@ -242,9 +250,11 @@ def drop_object(lynx, color, object_index, value):
         path = drop(qStart, color, [], 0, object_pose)
 
     else:
+        # sort the list and new list based on heights of objects stored in them
         list = sorted(list, key=operator.itemgetter(1))
         new_list = sorted(new_list, key=operator.itemgetter(1))
 
+        # set the initial target location based on the top object at p_i
         if(len(list) < 3 or len(new_list) == 0):
             target_pose = list[-1][0]
 
